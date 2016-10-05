352eff47140d7fe21d26d4e3d2008e2943285eba
smithan7 <smithan7@oregonstate.edu>
2016-10-05 06:40:36 -0700

    matlab code
    contains the main code which does gradient descent with L2 Regularization and batch testing.

---
+++ Implementation%201/batchGradient.m
@@ -0,0 +1,22 @@
+function [ w wSum gSum ] = batchGradient( w,h,y, epsilon, learningRate, lamda )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+    flag = 1;
+    i=0;
+    while flag
+        i = i+1;
+        yHat = h'*w;
+        err = y - yHat;
+        gradient = h*err / length(w);
+        gSum(i) = sum(gradient);
+        wSum(i) = sum(w);
+
+        w = w+learningRate*gradient - 2*lamda*w;
+        
+        if gSum(i) < epsilon
+            flag = 0;
+        end
+    end
+
+end
