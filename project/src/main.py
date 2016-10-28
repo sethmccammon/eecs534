b5677a3bdc40591f7554f7692da0ed2c8cb093e2
Omnei <sethm1093@gmail.com>
2016-10-28 14:27:17 -0700

    Project initial commit

---
+++ project/src/main.py
@@ -0,0 +1,11 @@
+
+
+#Read Data
+#I was thinking use a neural net to try and learn
+#we also can think about how to learn most informative features
+
+def main():
+  return 0
+
+if __name__ == '__main__':
+  main()
